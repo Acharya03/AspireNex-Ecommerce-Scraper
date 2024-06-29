2ac7a4bfd9110b4352cbf54a4d6cf09fab7dea9d
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-29 18:59:38 +0530

    home page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+	return (
+		<>
+			<section className="px-6 md:px-20 py-24 border-2 border-red-500">
+				<div className="flex max-xl:flex-col gap-16">
+					<div className="flex flex-col justify-center">
+						<p className="small-text">
+							Smart shopping starts here:
+							<Image
+								src="/assets/icons/arrow-right.svg"
+								alt="arrow-right"
+								width={16}
+								height={16}
+							/>
+						</p>
+						<h1 className="head-text">
+							Unleash the power of
+							<span className="text-primary"> PriceWatch</span>
+						</h1>
+						<p className="mt-6">
+							This will help you gain info and statistics regarding prices.
+						</p>
+						<Searchbar/>
+					</div>
+					HeroCarousel
+				</div>
+			</section>
+			<section className="trending-section">
+				<h2 className="section-text">
+					Trending
+				</h2>
+				<div className="flex flex-wrap gap-x-8 gap-y-16">
+					{['Apple Iphone 15', 'Book', 'Sneakers'].map((product) =>(
+						<div>
+							{product}
+						</div>
+					))}
+				</div>
+			</section>
+		</>
+  	)
 }
 
 export default Home
