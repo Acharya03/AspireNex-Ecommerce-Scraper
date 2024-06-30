4caf530915280a33c11917ddfaf556f8a120b752
Arunabho Bhattacharya <barunabho@gmail.com>
2024-06-30 23:52:41 +0530

    home page update

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -4,7 +4,14 @@ const Searchbar = () => {
 
     return (
         <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-
+            <input
+                type="text"
+                placeholder="enter product link"
+                className="searchbar-input"
+            />
+            <button className="searchbar-btn" type="submit">
+                Search
+            </button>
         </form>
 
     )
