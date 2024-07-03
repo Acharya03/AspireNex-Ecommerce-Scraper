34b3f3c6c063f615949003cc49bb3aff97994cea
Arunabho Bhattacharya <barunabho@gmail.com>
2024-07-03 16:00:19 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
-const Home = () => {
+const Home = async () => {
+
+	const allProducts = await getAllProducts();
+
 	return (
 		<>
 			<section className="px-6 md:px-20 py-24">
@@ -25,9 +30,9 @@ const Home = () => {
 						<p className="mt-6">
 							This will help you gain info and statistics regarding prices.
 						</p>
-						<Searchbar/>
+						<Searchbar />
 					</div>
-					<HeroCarousel/>
+					<HeroCarousel />
 				</div>
 			</section>
 			<section className="trending-section">
@@ -35,15 +40,14 @@ const Home = () => {
 					Trending
 				</h2>
 				<div className="flex flex-wrap gap-x-8 gap-y-16">
-					{['Apple Iphone 15', 'Book', 'Sneakers'].map((product) =>(
-						<div>
-							{product}
-						</div>
+					{allProducts?.map((product) => (
+						<ProductCard key={product._id} product={product}/>
+
 					))}
 				</div>
 			</section>
 		</>
-  	)
+	)
 }
 
 export default Home
